40ff1439434321d0413d3755404af9af3ed2b4f4
jimzhan <jim.zhan@me.com>
2016-07-23 12:43:24 +0800

    fix: missing index.html

--- etc/webpack.shared.js
+++ etc/webpack.shared.js
@@ -11,7 +11,7 @@ module.exports = {
   devtool: 'source-map',
 
   resolve: {
-    extensions: ['', '.css', '.js', '.jsx', '.scss']
+    extensions: ['', '.css', '.js', '.jsx', '.scss'],
   },
 
   output: {
